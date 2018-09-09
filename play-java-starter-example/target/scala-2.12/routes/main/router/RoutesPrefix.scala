fa9a46feb1738cdcea8fd225731cbd3ada2f9653
Chibashini Baskaran <chibashini2014@gmail.com>
2018-09-09 19:28:19 +0530

    @el-praveeno
    Error in Repository file

---
+++ play-java-starter-example/target/scala-2.12/routes/main/router/RoutesPrefix.scala
@@ -0,0 +1,15 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:H:/play-java-starter-example/play-java-starter-example/conf/routes
+// @DATE:Sun Jul 22 19:23:03 IST 2018
+
+
+package router {
+  object RoutesPrefix {
+    private var _prefix: String = "/"
+    def setPrefix(p: String): Unit = {
+      _prefix = p
+    }
+    def prefix: String = _prefix
+    val byNamePrefix: Function0[String] = { () => prefix }
+  }
+}
